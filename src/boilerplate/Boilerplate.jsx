2b7cb1f14d6444f81d798b4053f3db0950a5a715
Pattuv <161959486+Pattuv@users.noreply.github.com>
2025-12-30 10:24:29 -0500

    creating system for non-reacters

---
+++ src/boilerplate/Boilerplate.jsx
@@ -0,0 +1,13 @@
+// Change the function name to whatever your component is (Ex: Button, Navbar, etc.)
+function Boilerplate() {
+  return (
+    // This is where you write your HTML. When writing remember there can only be one parent html tag in the return().
+    <div>
+      This is an example React function. Copy this into your component start
+      coding!
+    </div>
+  );
+}
+
+// Copy this too! This allows your component to be used anywhere in the project
+export default Boilerplate;
